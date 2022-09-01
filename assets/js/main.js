8261b42d7f572186db3e2d6d99416aaaa06ca363
Bob <spocifyy@gmail.com>
2022-08-31 21:57:59 -0700

    Finalising site for Pages hosting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 var video = document.getElementById('coverLetterFootage');
+var pees  = document.querySelectorAll('.text-container p');
+var feets = document.querySelectorAll('footer li');
 
 // scroll function which enables video playback synchronously
 window.addEventListener('scroll', function(e) {
@@ -6,6 +8,25 @@ window.addEventListener('scroll', function(e) {
 
   // video duration is 4.375
   video.currentTime = wScroll/100;
+  console.log(wScroll);
 
   // fading in each paragraph
+  pees.forEach((el, i) => {
+    console.log(el.getBoundingClientRect().bottom, wScroll);
+    if(el.getBoundingClientRect().bottom < wScroll + 500) {
+      el.style.opacity = '1';
+    } else {
+      el.style.opacity = '0';
+    }
+  });
+
+  // fading in footer
+  feets.forEach((el, i) => {
+    console.log(el.getBoundingClientRect().bottom, wScroll);
+    if(el.getBoundingClientRect().bottom < wScroll + 300) {
+      el.style.opacity = '1';
+    } else {
+      el.style.opacity = '0';
+    }
+  })
 });
